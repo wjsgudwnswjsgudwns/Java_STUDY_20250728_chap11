80d78d1d01f3258d0cf2ed240e8e3a4c9bbc7658
wjsgudwnswjsgudwns <wns10099@naver.com>
2025-07-29 21:12:25 +0900

    4번

--- src/Ex/Ex_04.java
+++ src/Ex/Ex_04.java
@@ -5,14 +5,29 @@ public class Ex_04 {
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 
-		String str = "i like chopin";
-		
-		byte[] bytes = str.getBytes();
-		
-		
-		
-		System.out.println(str);
-		
+		   
+	     String str = "i like chopin";
+	     String sum = "";
+	     String sentence = "";
+	     
+
+	        for (int i = 0; i <= str.length(); i++) {
+	            if (i == str.length() || str.charAt(i) == ' ') {
+	                if (sentence.length() > 0) {
+	                    String capitalized = Character.toUpperCase(sentence.charAt(0)) + sentence.substring(1);
+	                    sum = sum + capitalized;
+	                    if (i < str.length()) {
+	                        sum = sum + " ";
+	                    }
+	                    sentence = "";
+	                }
+	            } 
+	            else {
+	            	sentence = sentence + str.charAt(i);
+	            }
+	        }
+
+	        System.out.println(sum);
 	}
 
 }
