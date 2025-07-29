c0420007b44e10e74e7b583b5d1c22bee72f8d32
wjsgudwnswjsgudwns <wns10099@naver.com>
2025-07-29 16:10:39 +0900

    java lang 패키지

---
+++ src/chap11/part01/String/MathTest.java
@@ -0,0 +1,26 @@
+package chap11.part01.String;
+
+public class MathTest {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		
+		System.out.println(Math.abs(-53.7745227)); // 절대값
+		
+		System.out.println(Math.ceil(14.547824)); // 올림
+		
+		System.out.println(Math.floor(48.55746841)); // 내림
+		
+		System.out.println(Math.round(8.521423)); // 반올림
+		
+		System.out.println(Math.max(5, 3)); // 두 수중 큰값 리턴
+		
+		System.out.println(Math.min(5, 3)); // 두 수중 작은값 리턴
+		
+		System.out.println(Math.random()); // 0<= i <1 랜덤 숫자
+		System.out.println(Math.random()); // 0<= i <1 랜덤 숫자
+		
+		System.out.println(Math.rint(9.57413684)); // 가장 가까운 정수 리턴
+	}
+
+}
---
+++ src/chap11/part01/String/WrapperTest.java
@@ -0,0 +1,16 @@
+package chap11.part01.String;
+
+public class WrapperTest {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+		Integer int1 = new Integer("1000");
+		System.out.println(int1.intValue()+1000);
+		
+		Integer int2 = 100;
+		
+		int a = Integer.parseInt("1000"); // 문자열 숫자를 숫자로 바꾸는 법
+	}
+
+}
